7d9d003cc9659efa56430f7b68e8865e3068718a
AValenzue <avzkitchen@gmail.com>
2025-04-16 22:04:23 +0200

    Refactor Dropdown component layout and styles for improved responsiveness and visual consistency

--- portfolio/src/components/project/Dropdown.jsx
+++ portfolio/src/components/project/Dropdown.jsx
@@ -3,37 +3,47 @@ import { useState } from "react";
 import "./Dropdown.scss";
 
 const Dropdown = ({ title, image, text, isFirst }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true); // Default to closed
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`dropdown ${isFirst ? "first-dropdown-layout" : ""}`}>
+    <div className="dropdown">
+      {/* Dropdown Header */}
       <div className="dropdown-header" onClick={toggleDropdown}>
         <h3>{title}</h3>
         <span className={`dropdown-icon ${isOpen ? "open" : ""}`}>+</span>
       </div>
+
+      {/* Dropdown Content */}
       {isOpen && (
-        <div className="dropdown-content">
-          {isFirst ? (
-            <div className="first-dropdown-layout">
-              {image && (
-                <div className="dropdown-image">
-                  <img src={image} alt={title} />
-                </div>
-              )}
+        <div
+          className={`dropdown-content ${
+            isFirst ? "image-left-text-right" : "image-right-text-left"
+          }`}
+        >
+          {/* Render Image and Text Based on Conditions */}
+          {isFirst && (
+            <>
+              <div className="dropdown-image">
+                <img src={image} alt={title} />
+              </div>
               <div className="dropdown-text">
                 <p>{text}</p>
               </div>
-            </div>
-          ) : (
-            <div className="dropdown-text">
-              <p>{text}</p>
-
-              {image && <img src={image} alt={title} />}
-            </div>
+            </>
+          )}
+          {!isFirst && (
+            <>
+              <div className="dropdown-text">
+                <p>{text}</p>
+              </div>
+              <div className="dropdown-image">
+                <img src={image} alt={title} />
+              </div>
+            </>
           )}
         </div>
       )}
