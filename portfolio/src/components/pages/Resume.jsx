852acec820c156b82d19cb8a257787676c37381b
AValenzue <avzkitchen@gmail.com>
2025-07-23 21:43:43 +0200

    update resume

--- portfolio/src/components/pages/Resume.jsx
+++ portfolio/src/components/pages/Resume.jsx
@@ -4,8 +4,7 @@ import React from "react";
 const Resume = () => {
   return (
     <div style={{ height: "100vh" }}>
-<iframe src="https://avzkitchen.notion.site/ebd/1ecee543e98380518252e8baac8a7d6e" width="100%" height="600" frameborder="0" allowfullscreen />
-    </div>
+<iframe src="https://avzkitchen.notion.site/ebd/1ecee543e98380518252e8baac8a7d6e" width="100%" height="600" frameborder="0" allowfullscreen />    </div>
   );
 };
 
