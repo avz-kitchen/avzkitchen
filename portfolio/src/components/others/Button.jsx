b001801c8aa80983f023024f6e06829cb6474ccf
AValenzue <avzkitchen@gmail.com>
2025-08-14 00:40:54 +0200

    Add Button component and corresponding styles: implement button functionality with primary and secondary variants

---
+++ portfolio/src/components/others/Button.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./Button.scss";
+
+const Button = ({ children, variant = "primary", to, href, ...props }) => {
+  const className = `btn ${variant}`;
+
+  if (to) {
+    return (
+      <Link to={to} className={className} {...props}>
+        {children}
+      </Link>
+    );
+  }
+
+  if (href) {
+    return (
+      <a href={href} className={className} {...props}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["primary", "secondary"]),
+  to: PropTypes.string,
+  href: PropTypes.string,
+};
+
+export default Button;
