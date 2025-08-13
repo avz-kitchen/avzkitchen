8d70bc0a7f577ef9be48f6753b6bf626180ef61e
AValenzue <avzkitchen@gmail.com>
2025-08-14 00:40:46 +0200

    Refactor ServiceSection component: update service titles and descriptions, enhance styling, and improve layout structure

--- portfolio/src/components/serviceSection/ServiceSection.jsx
+++ portfolio/src/components/serviceSection/ServiceSection.jsx
@@ -3,26 +3,28 @@ import GridLayout from "../others/GridLayout";
 
 const services = [
       {
-    title: "Brand Strategy",
-    content: "Defining product vision, roadmap, and user journeys.",
+    title: "Brand Strategy & Identity",
+    content: "I help brands find their voice and visual language, creating cohesive identities that tell a story and connect deeply with their audience. From logos and typography to color systems and messaging, I ensure every element reflects the brand’s purpose and values.",
   },
   {
     title: "Product Design",
-    content: "User experience and interface design for web and mobile apps.",
+    content: "User experience and designing accessible interfaces that prioritize usability for all users and scalable systems that ensure consistency across digital products",
   },
 
   {
     title: "Coding",
-    content: "Logo, color palette, and visual guidelines for your brand.",
+    content: "Bringing designs to life with HTML, CSS, JavaScript, React, and Next.js, ensuring responsive and high-performing websites.",
+  },
+    {
+    title: "Design Systems",
+    content: "Creating design systems that ensure consistency and scalability across products, enabling teams to deliver cohesive user experiences efficiently.",
   },
 ];
 
 const ServiceSection = () => (
   <section className="service-section">
     <GridLayout columns={2}>
-      <div>
-        <h2>Services</h2>
-      </div>
+        <h2>What I do</h2>
       <div>
         <ul className="service-list">
           {services.map((service, idx) => (
