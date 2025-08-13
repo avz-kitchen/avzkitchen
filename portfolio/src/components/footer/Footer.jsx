4bdbe784b601d9c42ea0a6b97ba17d1aa2cbe57f
AValenzue <avzkitchen@gmail.com>
2025-08-13 19:03:16 +0200

    Refactor Footer component: update layout, enhance accessibility, and improve styling

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -1,53 +1,27 @@
 import "./footer.scss";
 import { Link } from "react-router-dom";
-import GridLayout from "../others/GridLayout";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <ul className="span-three-column">
-        <li>
-          <h4>
-            <Link to="/portfolio/">Pictogram</Link>
-          </h4>
-        </li>
-        <li>
-          <h4>
-            <Link to="/portfolio/">Design</Link>
-          </h4>
-        </li>
-        <li>
-          <h4>
-            <Link to="/portfolio/">Code</Link>
-          </h4>
-        </li>
-      </ul>
+
       <div className="footer-content"></div>
+            <h1 className="xxxl">AVZKITCHEN</h1>
 
       {/* Sitemap Grid */}
       <div className="sitemap">
-        <div className="sitemap-category"> </div>
-        <div className="sitemap-category">
-          <Link to="/">
-            <h3>Visual Studio</h3>
-          </Link>
-          <Link to="/about">About AVZ</Link>
-          <Link to="/data">Data & Privacy</Link>
-        </div>
-        <div className="sitemap-category">
-          <Link to="/portfolio">
-            <h3>Portfolio</h3>
-          </Link>
+          <Link to="/bio">Bio</Link>
+          <Link to="/portfolio">Portfolio</Link>
           <Link to="/portfolio/#branding">Branding</Link>
           <Link to="/portfolio/#uxui">UX/UI Design</Link>
           <Link to="/portfolio/#webdev">Web Development</Link>
-        </div>
+          <Link to="/resume">Resume</Link>
+</div>
 
-        <div className="sitemap-category">
-          <Link to="/Contact">
-            <h3>Contact</h3>
-          </Link>
-          <Link to="/contact">Contact Me</Link>
+        <div className="contact-links">
+  <span className="social-links">
           <a
             href="https://github.com/avz-kitchen"
             target="_blank"
@@ -62,24 +36,30 @@ const Footer = () => {
           >
             LinkedIn
           </a>
-        </div>
-      </div>
+                    <a
+            href="https://www.instagram.com/artichoke.v/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          </span>
+                    <a href="mailto:avzkitchen@gmail.com">avzkitchen@gmail.com</a>
 
-      <section className="copyright">
-        <GridLayout columns={6}>
-          <h4 className="vertical-text span-two-column">
-            Some photos, <br /> some moving images
-          </h4>
-          <img src="/artichoke.png" className="span-four-column" />
-        </GridLayout>
+        </div>
+      <div className="copyright">
+          <Link to="/data">Data & Privacy</Link>
+
         <a
           href="https://www.instagram.com/artichoke.v/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          © 2025 @artichoke.v
+          © {year} avzkitchen. All rights reserved.
         </a>
-      </section>
+      <Link to="/legal">Legal</Link>
+
+      </div>
     </footer>
   );
 };
