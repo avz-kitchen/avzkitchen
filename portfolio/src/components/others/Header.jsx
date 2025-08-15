4a7a67dd5e0e5faabcf8805fb74315de61dada9c
AValenzue <avzkitchen@gmail.com>
2025-08-15 10:55:12 +0200

    Refactor Header component to improve text clarity and replace image with video for enhanced visual presentation; update Portfolio styles for better layout and responsiveness

--- portfolio/src/components/others/Header.jsx
+++ portfolio/src/components/others/Header.jsx
@@ -5,15 +5,25 @@ const Header = () => {
     <section >
       <GridLayout columns={4}>
         <h1 className="xxl span-three-columns">Hello there</h1>
-        <h4>I'm Angelica, welcome to the visual kitchen  for my<br/> creative digital outlets.. <br/> <Button variant="primary" to="/bio">Angelica's Bio</Button>
+        <h4>I'm Angelica, welcome to the visual kitchen, <br/> where ideas simmer. <Button variant="primary" to="/bio">Angelica's Bio</Button>
         </h4>
       </GridLayout >
-      <img
+      {/* <img
         src="public/project/flightapp/flight-app-hero.gif"
         alt="AVZ Kitchen" 
         height="720px"
         width="100%"
         style={{ objectFit: "cover", borderRadius: "8px" }} // Ensures the image covers the area without
+      /> */}
+  <video
+    src="https://res.cloudinary.com/dtleseja7/video/upload/avz-video.mp4"
+        alt="AVZ Kitchen"
+        height="720px"
+        width="100%"
+        style={{ objectFit: "cover",     objectPosition: "center", borderRadius: "8px", filter: "grayscale(100%)" }} // Ensures the video covers the area without distortion
+        autoPlay
+        loop
+        muted
       />
     </section>
   );
