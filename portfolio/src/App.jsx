809992d3bc8419728f664015fc75a80a6662d352
AValenzue <avzkitchen@gmail.com>
2025-08-19 14:41:05 +0200

    Add Helmet component for SEO optimization with title and meta description

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/pages/Home";
 import Portfolio from "./components/pages/Portfolio";
@@ -17,6 +18,33 @@ const App = () => {
   const projects = portfolioData.portfolio;
 
   return (
+    <>
+        <Helmet>
+        <title>Angelica Valenzuela | Freelance Product Designer & Developer</title>
+        <meta
+          name="description"
+          content="Angelica Valenzuela – Freelance Product Designer & Developer creating digital products, e-commerce solutions, and brands that leave a lasting impression."
+        />
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Angelica Valenzuela",
+            "url": "https://avzkitchen.com",
+            "jobTitle": "Freelance Product Designer & Developer",
+            "sameAs": [
+              "https://www.linkedin.com/in/avzkitchen",
+              "https://github.com/avz-kitchen",
+              "https://www.instagram.com/artichoke.v",
+              "https://www.twine.net/AngelicaValenzuela",
+              "https://www.xing.com/profile/Angelica_Valenzuela032612",
+              "https://www.behance.net/avzkitchen"
+            ]
+          }
+          `}
+        </script>
+      </Helmet>
     <Router>
       <ScrollToTop />
       <Navbar />
@@ -40,6 +68,7 @@ const App = () => {
       </div>
       <Footer />
     </Router>
+    </>
   );
 };
 
